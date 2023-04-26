4a18a8c879ed12b42be43cef615c535f584e8e60
Collin <cstruthe@uoguelph.ca>
2023-04-26 16:56:24 -0400

    Initial Commit

---
+++ Copify/CopifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopifyApp.swift
+//  Copify
+//
+//  Created by Collin Struthers on 2023-04-26.
+//
+
+import SwiftUI
+
+@main
+struct CopifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
